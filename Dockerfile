d06908923b0f860a66bbe2b1e66e53ee255f36d1
Airat-jv <airatul73@gmail.com>
2024-11-02 20:46:08 +0400

    get reply with incor encod ru symb

--- Dockerfile
+++ Dockerfile
@@ -10,9 +10,7 @@ COPY . /app
 # Установка зависимостей из файла requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Установка FastAPI и uvicorn
-RUN pip install fastapi uvicorn
-
 # Команда для запуска приложения
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", "--root-path", "/api"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+
 
--- main.py
+++ main.py
@@ -1,52 +1,50 @@
 from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
-
 import os
 import requests
 
-# Получаем OAuth токен из переменной окружения
+app = FastAPI()
+
 ACCESS_TOKEN = os.getenv("API_KEY")
 
 if ACCESS_TOKEN is None:
     raise ValueError("ACCESS_TOKEN is not set. Please set the API_KEY environment variable.")
 
-# Формирование URL для получения отзывов
 business_id = 3999348
 URL = f"https://api.partner.market.yandex.ru/v2/businesses/{business_id}/goods-feedback"
 
-# Заголовки для авторизации
 headers = {
     "Authorization": f"Bearer {ACCESS_TOKEN}",
     "Content-Type": "application/json"
 }
 
-# Параметры запроса - лимит количества отзывов
 params = {
     "limit": 1
 }
 
-# Тело запроса
 data = {
     "reactionStatus": "NEED_REACTION",
     "paid": False
 }
 
-# Выполнение POST запроса для получения одного последнего отзыва
-response = requests.post(URL, headers=headers, params=params, json=data)
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
 
-# Обработка ответа
-if response.status_code == 200:
-    result = response.json()
-    feedbacks = result.get("result", {}).get("feedbacks", [])
-    if feedbacks:
-        feedback = feedbacks[0]  # Получаем только один последний отзыв
-        print(feedback)  # Выводим последний отзыв
+@app.get("/feedback")
+async def get_feedback():
+    response = requests.post(URL, headers=headers, params=params, json=data)
+    response.encoding = 'utf-8'  # Устанавливаем кодировку
+
+    if response.status_code == 200:
+        result = response.json()
+        feedbacks = result.get("result", {}).get("feedbacks", [])
+        if feedbacks:
+            feedback = feedbacks[0]
+            return feedback
+        else:
+            return {"message": "Нет отзывов, требующих ответа."}
     else:
-        print("Нет отзывов, требующих ответа.")
-else:
-    print(f"Ошибка при получении отзывов: {response.status_code}, {response.text}")
+        return {
+            "error": f"Ошибка при получении отзывов: {response.status_code}",
+            "details": response.text
+        }

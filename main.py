1c34c06d86fbbee754895316e9fd2e85c5d83763
Airat-jv <airatul73@gmail.com>
2024-11-02 22:09:55 +0400

    get reply successfl on endpnt

--- main.py
+++ main.py
@@ -1,50 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 import os
 import requests
+import json
 
 app = FastAPI()
 
-ACCESS_TOKEN = os.getenv("API_KEY")
-
-if ACCESS_TOKEN is None:
-    raise ValueError("ACCESS_TOKEN is not set. Please set the API_KEY environment variable.")
-
-business_id = 3999348
-URL = f"https://api.partner.market.yandex.ru/v2/businesses/{business_id}/goods-feedback"
-
-headers = {
-    "Authorization": f"Bearer {ACCESS_TOKEN}",
-    "Content-Type": "application/json"
-}
-
-params = {
-    "limit": 1
-}
-
-data = {
-    "reactionStatus": "NEED_REACTION",
-    "paid": False
-}
-
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
-
 @app.get("/feedback")
-async def get_feedback():
+def get_feedback():
+    ACCESS_TOKEN = os.getenv("API_KEY")
+    if ACCESS_TOKEN is None:
+        raise ValueError("ACCESS_TOKEN is not set. Please set the API_KEY environment variable.")
+
+    business_id = 3999348
+    URL = f"https://api.partner.market.yandex.ru/v2/businesses/{business_id}/goods-feedback"
+
+    headers = {
+        "Authorization": f"Bearer {ACCESS_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    params = {
+        "limit": 1
+    }
+
+    data = {
+        "reactionStatus": "NEED_REACTION",
+        "paid": False
+    }
+
     response = requests.post(URL, headers=headers, params=params, json=data)
-    response.encoding = 'utf-8'  # Устанавливаем кодировку
 
     if response.status_code == 200:
-        result = response.json()
-        feedbacks = result.get("result", {}).get("feedbacks", [])
-        if feedbacks:
-            feedback = feedbacks[0]
-            return feedback
-        else:
-            return {"message": "Нет отзывов, требующих ответа."}
+        feedback_data = response.json()
+        json_response = json.dumps(feedback_data, ensure_ascii=False)
+        return Response(content=json_response, media_type="application/json; charset=utf-8")
     else:
-        return {
-            "error": f"Ошибка при получении отзывов: {response.status_code}",
-            "details": response.text
-        }
+        return {"error": f"Ошибка при получении отзывов: {response.status_code}, {response.text}"}
